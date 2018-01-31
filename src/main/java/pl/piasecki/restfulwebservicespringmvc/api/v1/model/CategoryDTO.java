e9bc0100f957d3f40df013cb8b42e09a11181636
Patrick <piasecki.patrick.15@gmail.com>
2018-01-31 14:21:24 +0100

    adding Category entity, repo and DTO

---
+++ src/main/java/pl/piasecki/restfulwebservicespringmvc/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package pl.piasecki.restfulwebservicespringmvc.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/pl/piasecki/restfulwebservicespringmvc/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package pl.piasecki.restfulwebservicespringmvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.piasecki.restfulwebservicespringmvc.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+}
