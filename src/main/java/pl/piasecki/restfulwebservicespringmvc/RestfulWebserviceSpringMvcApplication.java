88ea8ccf60cccd0d8b2209021a6cca3324f79f4c
Patrick <piasecki.patrick.15@gmail.com>
2018-01-31 13:11:39 +0100

    second commit

---
+++ src/main/java/pl/piasecki/restfulwebservicespringmvc/RestfulWebserviceSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package pl.piasecki.restfulwebservicespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviceSpringMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/piasecki/restfulwebservicespringmvc/RestfulWebserviceSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.piasecki.restfulwebservicespringmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebserviceSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
